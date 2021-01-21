f56049de630faec989d6e0b63c0b892fb0da3ee2
Mohamed Sameer <74092838+Sameeeeeeeer@users.noreply.github.com>
2021-01-21 15:45:33 +0500

    layout the navigation component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./nav.css";
+import { IconContext } from "react-icons";
+import { BiFootball } from "react-icons/bi";
 
 const Navbar = () => {
-  return <div> I am Nav</div>;
+  return (
+    <>
+      <nav className="nav-container">
+        <div className="nav-left">
+          <ul>
+            <li>
+              <IconContext.Provider value={{ size: "2em" }}>
+                <BiFootball />
+              </IconContext.Provider>
+            </li>
+            <li>Player</li>
+            <li>Match</li>
+            <li>Ranking</li>
+          </ul>
+        </div>
+        <div className="nav-right">
+          <ul>
+            <li>icon</li>
+            <li>login</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
